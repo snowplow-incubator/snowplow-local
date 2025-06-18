aabe85cece3549db7ce04184bfe42183157cdd08
Mike Robins <mike@poplindata.com>
2025-06-18 14:10:58 +1000

    Update UI and control plane for schema management

--- control-plane/app.py
+++ control-plane/app.py
@@ -2,6 +2,7 @@ from base64 import b64decode, b64encode
 from glob import glob
 import json
 import logging
+import os
 
 import docker
 import requests
@@ -94,5 +95,71 @@ def write_enrichment():
         file.write(json.dumps(data, indent=4))
     restart('snowplow-enrich')
     return {'success': True}
+
+## Schema API (local only)
+
+def get_all_schemas() -> list[str]:
+    # returns the contents of all schemas in the local repository
+    r = requests.get('http://host.docker.internal:8081/api/schemas',
+    headers={
+        'apikey': os.environ.get('IGLU_SUPER_API_KEY', '')
+    })
+    print('response:', r.text)
+    r.raise_for_status()
+    return r.json()
+
+def get_schema(iglu_uri: str, return_metadata: bool = True) -> dict:
+    # todo: whether to return metadata or not...
+    if return_metadata:
+        repr = 'meta'
+    else:
+        repr = 'canonical'
+    # returns a single schema by its Iglu URI
+    iglu_url = iglu_uri.replace('iglu:', '')
+    r = requests.get(f'http://host.docker.internal:8081/api/schemas/{iglu_url}',
+        params={
+            'repr': repr
+        },               
+        headers={
+            'apikey': os.environ.get('IGLU_SUPER_API_KEY', '')
+        })
+    r.raise_for_status()
+    return r.json()
+
+@app.route('/schemas', methods=['POST'])
+def write_schema():
+    # writes (or overwrites) a schema to the local repo
+    schema = request.json
+    print('schema: ', schema)
+    # use the Iglu Server API to upload the schema
+    r = requests.post('http://host.docker.internal:8081/api/schemas', json=schema,
+    headers={
+        'apikey': os.environ.get('IGLU_SUPER_API_KEY', '')
+    })
+    print('response:', r.text)
+    r.raise_for_status()
+    return r.json()
+
+@app.route('/schemas', methods=['GET'])
+def list_schemas():
+    schema_list = get_all_schemas()
+    schemas = []
+    for schema in schema_list:
+        schemas.append(get_schema(schema, return_metadata=True))
+    return schemas
+
+@app.route('/validate', methods=['POST'])
+def validate_schema():
+    schema = request.json
+    r = requests.post('http://host.docker.internal:8081/api/validation/validate/schema/jsonschema', json=schema,
+    headers={
+        'apikey': os.environ.get('IGLU_SUPER_API_KEY', '')
+    })
+    # r.raise_for_status() # do not raise
+    return r.json()
+
+
+
+
     
         
